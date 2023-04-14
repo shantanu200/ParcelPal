a30e026d38b3b2d91edd3aaf0eeccffec983fb4f
Shantanu Bhusari <84804589+shantanu200@users.noreply.github.com>
2023-04-15 00:53:11 +0530

    15 April Employee and CourierPackage Schema added

---
+++ server/models/CourierPackages.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+const courierPackageSchema = new mongoose.Schema({
+  senderName: {
+    type: String,
+    required: true,
+  },
+  senderAddress: {
+    type: String,
+    required: true,
+  },
+  receiverName: {
+    type: String,
+    required: true,
+  },
+  receiverAddress: {
+    type: String,
+    required: true,
+  },
+  weight: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["Pending", "In Transit", "Delivered"],
+    default: "Pending",
+  },
+  deliveryDate: {
+    type: Date,
+  },
+  deliveryPerson: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Employee",
+  },
+  scheduledShipment: {
+    date: {
+      type: Date,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+  },
+});
+
+export default mongoose.model("CourierPackage", courierPackageSchema);
---
+++ server/models/Employee.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const Employee = new mongoose.Schema({
+  name: {
+    type: String,
+    requried: true,
+  },
+  email: {
+    type: String,
+    requried: true,
+  },
+  password: {
+    type: String,
+    requried: true,
+  },
+  role: {
+    type: String,
+    enum: ["Admin", "Manager", "Delivery Person"],
+    default: "Delivery Person",
+  },
+  assignedPackages: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "CourierPackage",
+    },
+  ],
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  isActive:{
+    type:Boolean
+  }
+},{timestamps:true});
+
+
+export default mongoose.model("Employees",Employee);
